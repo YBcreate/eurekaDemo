bb840fc01e04a0638480303306b9fdf2c8fa4e2c
YBcreate <805313212@qq.com>
2019-12-14 16:02:50 +0800

    eureka高可用本地例子

---
+++ eureka2/src/test/java/com/xieyongbin/eureka2/Eureka2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xieyongbin.eureka2;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eureka2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
